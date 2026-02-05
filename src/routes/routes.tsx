e79d3be845bf9f2f6b604fbc91b01cf44214e26c
Shibu-Patil <patilshibu@gmail.com>
2026-02-05 18:26:18 +0530

    added git

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,25 +1,28 @@
- import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import UserPages from "../pages/UserPages";
 import DashBoard from "../pages/DashBoard";
 import UpdatePage from "../pages/UpdatePage";
 import DisplayUser from "../pages/DisplayUser";
 
- const routes=createBrowserRouter([{
-    path:"/",
-    element:<DashBoard></DashBoard>,
-    children:[
-        {
-            index:true,
-            element:<UserPages></UserPages>
-        },{
-            path:"/update",
-            element:<UpdatePage></UpdatePage>
-        },{
-            path:"/display",
-            element:<DisplayUser></DisplayUser>
-        }
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashBoard />,
+    children: [
+      {
+        index: true,
+        element: <UserPages />
+      },
+      {
+        path: "update",
+        element: <UpdatePage />
+      },
+      {
+        path: "display",
+        element: <DisplayUser />
+      }
     ]
- }])
+  }
+]);
 
-
- export default routes
+export default routes;
