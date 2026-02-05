703769a6eaf3727235c6970c604e57a32ac3adba
Shibu-Patil <patilshibu@gmail.com>
2026-02-05 18:56:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { RouterProvider } from "react-router-dom"
+import { HashRouter, RouterProvider } from "react-router-dom"
 import "./styles/style.css"
 import routes from "./routes/routes"
 import { Toaster } from "react-hot-toast";
@@ -7,7 +7,9 @@ import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
   <>
-      <RouterProvider router={routes}></RouterProvider>
+      <HashRouter>
+        <RouterProvider router={routes}></RouterProvider>
+      </HashRouter>
       <Toaster></Toaster>
   </>
   )
